6ac88be382cde38426b8de7c3d685272b449e1c1
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-04-26 17:47:39 +0200

    Refactor stations handling

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,13 @@
 const args = require('args')
 const downloadReadings = require('../lib')
+const Options = require('../lib/options')
 
-args.option(['d', 'date'], 'Read air quality data for specific date')
+args.option(['d', 'date'], 'Read air quality data for specific date', Options.default.date)
+args.option(['s', 'station'], 'Read air quality data for specific station(s)', Options.default.station)
 args.example('monitoringkrakowpiosgovpl-dl', 'To fetch data from today')
 args.example('monitoringkrakowpiosgovpl-dl -d "2018-01-22"', 'To fetch data from January 22nd')
+args.example('monitoringkrakowpiosgovpl-dl -s telimeny', 'To fetch data from one station')
+args.example('monitoringkrakowpiosgovpl-dl -s telimeny -s nowahuta -s krasinskiego', 'To fetch data from multiple stations')
 const opts = args.parse(process.argv)
 
 downloadReadings((err, data) => {
@@ -11,5 +15,6 @@ downloadReadings((err, data) => {
     console.log(`${data.station.name}, ${data.param.label}, ${data.reading[1]}`)
   }
 },{
-  date: opts.date || new Date()
+  date: opts.date,
+  station: opts.station
 })
