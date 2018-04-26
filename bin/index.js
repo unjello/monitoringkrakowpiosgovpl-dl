66d1dda0dac1fc86dce27999d6cdfd021b44212d
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-04-26 17:54:21 +0200

    Make linter happy

--- bin/index.js
+++ bin/index.js
@@ -14,7 +14,7 @@ downloadReadings((err, data) => {
   if (!err) {
     console.log(`${data.station.name}, ${data.param.label}, ${data.reading[1]}`)
   }
-},{
+}, {
   date: opts.date,
   station: opts.station
 })
