5f5f455de357ddb28379e9b001e0de56d7c3b407
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-05-01 13:38:38 +0200

    Refactor reporter binary

--- bin/index.js
+++ bin/index.js
@@ -8,10 +8,23 @@ args.example('monitoringkrakowpiosgovpl-dl', 'To fetch data from today')
 args.example('monitoringkrakowpiosgovpl-dl -d "2018-01-22"', 'To fetch data from January 22nd')
 args.example('monitoringkrakowpiosgovpl-dl -s telimeny', 'To fetch data from one station')
 args.example('monitoringkrakowpiosgovpl-dl -s telimeny -s nowahuta -s krasinskiego', 'To fetch data from multiple stations')
-const opts = args.parse(process.argv)
 
 const main = async () => {
+  const opts = args.parse(process.argv)
   const ratings = await Promise.all(await downloadReadings({ date: opts.date, station: opts.station }))
+  for (const s of ratings) {
+    console.log(`ℹ️  ${s.station.name}`)
+    console.group()
+    for (const c of s.data) {
+      console.log(`🔸  ${c.label}`)
+      console.group()
+      for (const r of c.data) {
+        console.log(`${r.value}`)
+      }
+      console.groupEnd()
+    }
+    console.groupEnd()
+  }
 }
 
 main()
