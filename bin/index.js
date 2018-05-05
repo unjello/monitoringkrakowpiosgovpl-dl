b6a1edd454a853e59bbc54d8dffbaa7b1d22542d
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-05-05 20:25:08 +0200

    Refactoring

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,6 @@
 const args = require('args')
 const downloadReadings = require('../lib')
+const structureData = require('../lib/structureData')
 const Options = require('../lib/options')
 const chalk = require('chalk')
 const { table, getBorderCharacters } = require('table')
@@ -13,44 +14,18 @@ args.example('monitoringkrakowpiosgovpl-dl -d "2018-01-22"', 'To fetch data from
 args.example('monitoringkrakowpiosgovpl-dl -s telimeny', 'To fetch data from one station')
 args.example('monitoringkrakowpiosgovpl-dl -s telimeny -s nowahuta -s krasinskiego', 'To fetch data from multiple stations')
 args.example('monitoringkrakowpiosgovpl-dl -s telimeny -s nowahuta -t', 'Prints out tabbular data')
-const getLimit = code => {
-  if (code === 'PM10') return 50;
-  if (code === 'PM2.5') return 25;
-  throw new Error('unknown parameter')
-}
+
 
 const main = async () => {
   const opts = args.parse(process.argv)
   const ratings = await Promise.all(await downloadReadings({ date: opts.date, station: opts.station }))
-
-  let data = {
-    byCode: {},
-    byStation: {}
-  }
-
-  for (const s of ratings) {
-    for (const c of s.data) {
-      if (!data.byCode[c.code]) {
-        data.byCode[c.code] = []
-      }
-      if (!data.byCode[c.code][0]) {
-        data.byCode[c.code][0] = []
-      }
-      data.byCode[c.code][0].push(s.station.name)
-      const limit = getLimit(c.code)
-      for (i = 0, j = 1; i < c.data.length; i++, j++) {
-        if (!data.byCode[c.code][j]) {
-          data.byCode[c.code][j] = []
-        }
-        data.byCode[c.code][j].push(`${formatColor(limit, (parseFloat(c.data[i].value)).toFixed(1))}`)
-      }
-    }
-  }
-  for (const code of Object.keys(data.byCode)) {
-    padColumns(data.byCode[code], chalk.gray('-'))
-  }
+  const data = structureData(ratings, (limit, value) => `${formatColor(limit, (parseFloat(value)).toFixed(1))}`)
 
   if (opts.t) {
+    for (const code of Object.keys(data.byCode)) {
+      padColumns(data.byCode[code], chalk.gray('-'))
+    }
+  
     options = {
       drawHorizontalLine: (index, size) => index === 0 || index === 1 || index === size,
       border: getBorderCharacters('norc')
